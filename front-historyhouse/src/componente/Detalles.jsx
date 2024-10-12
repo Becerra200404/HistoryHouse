41fb6697ffabee526dd66e94b0827b787bdc363b
alexander1755 <dylanalex.becerraricaldi2014@gmail.com>
2024-10-12 13:55:02 -0400

    Se implemento el consumo de las APIs

--- front-historyhouse/src/componente/Detalles.jsx
+++ front-historyhouse/src/componente/Detalles.jsx
@@ -1,25 +1,26 @@
 
 
-const Detalles = () => {
+const Detalles = ({ details , categorys}) => {
+  const categorias = categorys.map(libro => libro.nombre_categoria).join(', ');
   return (
     <div className="container-detalle">
       <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12" style={{ backgroundColor: '#E8E0C8' }}>
         <h2 style={{ textAlign: 'center' }}>Detalles del libro</h2>
         <div className="container-fluid detalles-container">
           <div className="detalles-box">
-            <p className="card-text mb-auto">Autor: </p>
+            <p className="card-text mb-auto">Autor: {details.nombre_autor}</p>
           </div>
           <div className="detalles-box">
-            <p className="card-text mb-auto">Editorial: </p>
+            <p className="card-text mb-auto">Editorial: {details.nombre_editorial}</p>
           </div>
           <div className="detalles-box">
-            <p className="card-text mb-auto">Número de páginas: </p>
+            <p className="card-text mb-auto">Número de páginas: {details.numero_paginas}</p>
           </div>
           <div className="detalles-box">
-            <p className="card-text mb-auto">Fecha de publicación: </p>
+            <p className="card-text mb-auto">Fecha de publicación: {details.fecha_publicacion}</p>
           </div>
           <div className="detalles-box">
-            <p className="card-text mb-auto">Género: </p>
+            <p className="card-text mb-auto">Categorias: {categorias} </p>
           </div>
         </div>
       </div>
--- front-historyhouse/src/inicio/Home.jsx
+++ front-historyhouse/src/inicio/Home.jsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import './Inicio.css'
 import { useState, useEffect } from "react";
 import { getBooks } from "../service/api";
 
 const Home = () => {
-
+  
   
   const [books, setBooks] = useState([]);
+  const navigate = useNavigate(); 
 
   console.log("Estos son los libros",books)
 
@@ -20,23 +21,30 @@ const Home = () => {
       const response = await getBooks();
       console.log(response.data)
       setBooks(response.data);
+     
     } catch (error) {
       console.error(error);
     }
-  }
+  };
+
+  const irADetalles = (id) => {
+    navigate(`/Libro/${id}`);
+  };
+
 
   return (
+    
     <div>
-
-
         <Link className='menu-link' to="/Libro"><h1>ir  a los detalles</h1></Link>
         {
-          books.map((ale) => (
-            <div className="card" key = {ale.id_libro} style={{ backgroundImage: "url('/assets/img.jpg')" }}>
-            <img className="img" src={ale.portada_libro} height={'200px'} width={'150px'}></img>
+          books.map((book) => (
+            <div className="card" key = {book.id_libro} style={{ backgroundImage: "url('/assets/img.jpg')" }}>
+            <img className="img" src={book.portada_libro} height={'200px'} width={'150px'}></img>
               <div className="textos">
-                <Link className='menu-link' to="/Libro"><h3>ir  a los detalles</h3></Link>
-               <p>{ale.titulo_libro}</p>
+                <button className='menu-link' onClick={() => irADetalles(book.id_libro)}>
+                  <h3>Ir a los detalles</h3>
+                </button>
+               <p>{book.titulo_libro}</p>
             </div>
           </div>
 
